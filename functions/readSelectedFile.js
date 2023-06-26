f074a96ef7bdc7e6f2781bdb4703329114dcf52b
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-06-27 00:19:14 +0400

    refactor: catch error in readFile

--- functions/readSelectedFile.js
+++ functions/readSelectedFile.js
@@ -3,14 +3,22 @@ import fs from 'fs';
 import showMessage from './showMessage.js';
 
 export default async function readSelectedFile(pathToFile) {
-  if (!fs.existsSync(pathToFile) || !isFile(pathToFile)) {
+  try {
+    if (!fs.existsSync(pathToFile) || !isFile(pathToFile)) {
+      showMessage();
+      return;
+    }
+
+    const readStream = fs.createReadStream(pathToFile, { encoding: 'utf-8' });
+
+    readStream.on('data', (chunk) => {
+      console.log(chunk);
+    });
+
+    readStream.on('error', (err) => {
+      showMessage();
+    });
+  } catch (err) {
     showMessage();
-    return;
   }
-
-  const readStream = fs.createReadStream(pathToFile, { encoding: 'utf-8' });
-
-  readStream.on('data', (chunk) => {
-    console.log(chunk);
-  });
 }
