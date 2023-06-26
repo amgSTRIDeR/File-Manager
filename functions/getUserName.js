b6097644423a7f96e7200e1ec9cc541221e2c341
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-06-26 17:00:38 +0400

    feat: user by userInfo if no username argument

--- functions/getUserName.js
+++ functions/getUserName.js
@@ -1,9 +1,13 @@
+import os from 'os';
+
 export default function getUserName() {
   const commandLineArgs = process.argv.slice(2);
   const userName =
     commandLineArgs
       .filter((arg) => arg.startsWith('--username='))
-      .map((arg) => arg.slice(11))[0] || 'Unknown User';
+      .map((arg) => arg.slice(11))[0] ||
+    os.userInfo().username ||
+    'Unknown User';
 
   return userName;
 }
