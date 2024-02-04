cf9b3a26ec99dd986b1b994f3064d0fbb6c32e09
amgstrider <amgstrider@gmail.com>
2024-02-04 14:01:37 +0400

    fix: invalid input if no text after cs command

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import { stdin } from "process"
 import os from "os";
 import * as utils from "./_imports.js";
 
+function isTargetPathExist(targetPath) {
+    if (!targetPath) {
+        utils.printInConsole('Invalid input', 'red');
+        return false;
+    }
+    return true;
+}
+
 const username = utils.getUsername();
 let currentDirectory = os.homedir();
 //develop
@@ -10,21 +18,26 @@ utils.greetUser(username, currentDirectory);
 
 stdin.on('data', async (data) => {
     const preparedData = data.toString().trim().split(' ');
+    const targetPath = preparedData.slice(1).join(' ');
+
     switch (preparedData[0]) {
         case ('up'):
             currentDirectory = utils.getUpperDirectory(currentDirectory);
             break;
         case ('cd'):
-            const targetPath = preparedData.slice(1).join(' ');
-            if (targetPath === undefined) {
-                utils.printInConsole('Invalid input', 'red');
-                break;
+            if (isTargetPathExist(targetPath)) {
+                currentDirectory = await utils.getDirectoryPath(currentDirectory, targetPath);
             }
-            currentDirectory = await utils.getDirectoryPath(currentDirectory, targetPath);
             break;
         case ('ls'):
             await utils.showList(currentDirectory);
             break;
+        // case ('cat'):
+        //     if (targetPath === undefined) {
+        //         utils.printInConsole('Invalid input', 'red');
+        //         break;
+        //     }
+        //     break;
         case ('.exit'):
             utils.sayGoodbye(username);
             process.exit();
